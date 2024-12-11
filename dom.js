92a027af7531baa21a9fb5aa6ea3394bbe7923a0
howcode0 <kadekisaka28@icloud.com>
2024-12-12 05:38:16 +0800

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,66 @@
+const apiKey = '891060519296be31fbe8978dde07a559'; //api key dari weather map jangan di ubah 
+//openweathermap.org
+
+function getWeather() {
+    const city = document.getElementById('city').value;
+    
+    //jika kota tidak ada ini yang muncul boleh di ubah jika berpengaruh sama css nya
+    if (!city) {
+        document.getElementById('weatherResult').innerHTML = '<p>Nama kota harus diisi!</p>';
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=id`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // debug api yang akan di tampilkan di indexOF bisa cek di console
+            if (data.cod === 200) { //200 OK 400 EROR 500 EROR Tanggapan server jika overflow (maxload)
+                const temperature = data.main.temp; //temperatur celcius
+                const description = data.weather[0].description; //kondisi cuaca 
+                const humidity = data.main.humidity; //kelembapan suatu daerah
+                const windSpeed = data.wind.speed; //kecepatan angin m/s
+                const cloudiness = data.clouds.all; // Persentase awan
+
+                document.getElementById('weatherResult').innerHTML = `
+                    <p id="temperature" >Suhu: ${temperature}°F</p>  
+                    <p id="kondisi">Kondisi: ${description}</p>
+                    <p id="kelembapan">Kelembapan: ${humidity}%</p>
+                    <p id="angin">Kecepatan Angin: ${windSpeed} m/s</p>
+                    <p id="awan">Persentase Awan: ${cloudiness}%</p>
+                `; //udah pake id jangan lupa # di css
+                
+                // Cek kondisi cuaca dan beri peringatan
+                const condition = description.toLowerCase();
+                
+                //alert boleh di ubah kalau tahu kondisi
+
+                if (condition.includes("drizzle")) {
+                    alert("Waspada! Hujan Rintik-rintik.");
+                }
+                if (condition.includes("rain")) {
+                    alert("Waspada! Hujan Deras.");
+                }
+                if (condition.includes("thunderstorm")) {
+                    alert("Waspada! Badai Petir.");
+                }
+                if (cloudiness >= 50) {
+                    alert("Waspada! Awan .");
+                }
+                if (cloudiness < 50 && cloudiness > 20) {
+                    alert("Waspada! Awan Tersebar.");
+                }
+
+                //muncul kalo kota ga ada
+            } else {
+                document.getElementById('weatherResult').innerHTML = `<p>Kota tidak ditemukan!</p>`;
+            }
+        })
+
+        //muncul kalau api bermasalah atau kesalah kondisi cuaca
+        .catch(error => {
+            document.getElementById('weatherResult').innerHTML = `<p>Terjadi kesalahan. Silakan coba lagi!</p>`;
+            console.log(error);
+        });
+}
---
+++ readme.txt
@@ -0,0 +1,21 @@
+PROJECT KELAS X MEMBUAT WEBSITE CUACA DENGAN MENGUNAKAN API KEY DARI openweathermap.org
+
+API key berguna sebagai pencari kota dan kondisi cuaca 
+system buka orisinil dari kami kami hanya mengunakan api open source
+
+Pengembang 
+-saka
+-dwi
+
+backend
+-isaka
+
+frontend
+-dwi
+
+tujuan membuat memberi experience kepada user
+agar memdapatkan informasi seputar cuaca di kota mereka dengan cepat dan sistem yang simple
+minim overload karena sistem kami simple
+
+Mengunakan fungsi fetc JS
+yang memungkinkan gampang di ubah array nya oleh siapa saja
